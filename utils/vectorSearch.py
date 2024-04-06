d4fad4d0a4dfe5cd444a21e54339e56671ef2704
Lennard Dorst <79471472+0w9@users.noreply.github.com>
2024-04-06 14:25:46 -0700

    Adding langchain vector search

--- utils/vectorSearch.py
+++ utils/vectorSearch.py
@@ -1,14 +1,18 @@
-import pymongo
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import MongoDBAtlasVectorSearch
+from langchain_openai import OpenAIEmbeddings
+from pydantic import AnyHttpUrl
+from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
-
-from langchain_openai import OpenAIEmbeddings
-
-from langchain.prompts import PromptTemplate
-# from langchain.output_parsers import PydanticOutputParser
-from langchain_core.output_parsers import JsonOutputParser, StrOutputParser, PydanticOutputParser
-import os
-
 load_dotenv()
 
-os.environ['LANGCHAIN_TRACING_V2'] = 'true'
+vector_search = MongoDBAtlasVectorSearch.from_connection_string(
+    f"mongodb+srv://{os.environ.get('MONGODB_LOGIN_USERNAME')}:{os.environ.get('MONGODB_LOGIN_PASSWORD')}@hackathon.0h4onx0.mongodb.net/?retryWrites=true&w=majority&appName=Hackathon",
+    OpenAIEmbeddings(disallowed_special=()),
+    index_name="text_index",
+)
+results = vector_search.similarity_search_with_score(
+    query="Meetups i went to ",
+    k=5,
+)
