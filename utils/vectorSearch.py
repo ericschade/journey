cf131ad5dea20993d84ef0ecb71dfeb7d4ddc985
Leo Walker <leowalker89@gmail.com>
2024-04-06 15:02:13 -0700

    updated the VectorSearch
    turned the vector search into a function

--- utils/vectorSearch.py
+++ utils/vectorSearch.py
@@ -6,13 +6,17 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
-vector_search = MongoDBAtlasVectorSearch.from_connection_string(
-    f"mongodb+srv://{os.environ.get('MONGODB_LOGIN_USERNAME')}:{os.environ.get('MONGODB_LOGIN_PASSWORD')}@hackathon.0h4onx0.mongodb.net/?retryWrites=true&w=majority&appName=Hackathon",
-    "memories.memoriesCollection",
-    OpenAIEmbeddings(disallowed_special=()),
-    index_name=ATLAS_VECTOR_SEARCH_INDEX_NAME,
-)
-results = vector_search.similarity_search_with_score(
-    query="Meetups i went to ",
-    k=5,
-)
+
+def queryVectorSearch(query):
+    vector_search = MongoDBAtlasVectorSearch.from_connection_string(
+        f"mongodb+srv://{os.environ.get('mongodb_uri')}",
+        "memories.memoriesCollection",
+        OpenAIEmbeddings(disallowed_special=()),
+        index_name=ATLAS_VECTOR_SEARCH_INDEX_NAME,
+    )
+    results = vector_search.similarity_search_with_score(
+        query=query,
+        k=5,
+    )
+    
+    return results
