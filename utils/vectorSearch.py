f9c368e4ffb78e13d9adef2a50136bc34151eddb
Leo Walker <leowalker89@gmail.com>
2024-04-06 14:31:31 -0700

    Updated the vector search to have a get_memories function.

--- utils/vectorSearch.py
+++ utils/vectorSearch.py
@@ -7,12 +7,16 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-vector_search = MongoDBAtlasVectorSearch.from_connection_string(
-    f"mongodb+srv://{os.environ.get('MONGODB_LOGIN_USERNAME')}:{os.environ.get('MONGODB_LOGIN_PASSWORD')}@hackathon.0h4onx0.mongodb.net/?retryWrites=true&w=majority&appName=Hackathon",
-    OpenAIEmbeddings(disallowed_special=()),
-    index_name="text_index",
-)
-results = vector_search.similarity_search_with_score(
-    query="Meetups i went to ",
-    k=5,
-)
+def get_memories(query):
+        
+    vector_search = MongoDBAtlasVectorSearch.from_connection_string(
+        f"mongodb+srv://{os.getenv('mongodb_uri')},
+        OpenAIEmbeddings(disallowed_special=()),
+        index_name="text_index",
+    )
+    results = vector_search.similarity_search_with_score(
+        query,
+        k=5,
+    )
+    
+    return results
